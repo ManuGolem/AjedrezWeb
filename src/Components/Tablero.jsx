089742bc95c836557b5d78ae160d67c7d900e095
ManuGolem <manuelcarreras2013@gmail.com>
2025-03-27 23:53:06 -0300

    update

--- src/Components/Tablero.jsx
+++ src/Components/Tablero.jsx
@@ -1,67 +1,33 @@
-import { useEffect } from "react";
 import data from "../dictionary.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useStartTablero } from "../Hooks/useStartTablero";
 export function Tablero() {
     const [mapPosPiezas, setMapPiezas] = useState();
     const [tablero, setTablero] = useState();
+    const [posibles, setPosibles] = useState();
+    const { tableroCords, piezas } = useStartTablero();
     useEffect(() => {
-        let tableroAux = Array(8)
-            .fill(null)
-            .map(() => Array(8).fill(""));
-        const arregloUtil = [
-            [8, 7, 6, 5, 4, 3, 2, 1],
-            ["a", "b", "c", "d", "e", "f", "g", "h"],
-        ];
-        arregloUtil[0].forEach((columna, indexcol) => {
-            arregloUtil[1].forEach((fila, indexfila) => {
-                tableroAux[indexcol][indexfila] = `${fila}${columna}`;
-            });
-        });
-        //Seteo el tablero con sus cords
-        setTablero(tableroAux);
-        setMapPiezas({
-            a1: "r",
-            b1: "c",
-            c1: "b",
-            d1: "q",
-            e1: "k",
-            f1: "b",
-            g1: "c",
-            h1: "r",
-            a2: "p",
-            b2: "p",
-            c2: "p",
-            d2: "p",
-            e2: "p",
-            f2: "p",
-            g2: "p",
-            h2: "p",
-            a7: "P",
-            b7: "P",
-            c7: "P",
-            d7: "P",
-            e7: "P",
-            f7: "P",
-            g7: "P",
-            h7: "P",
-            a8: "R",
-            b8: "C",
-            c8: "B",
-            d8: "Q",
-            e8: "K",
-            f8: "B",
-            g8: "C",
-            h8: "R",
-        });
-    }, []);
-
+        setTablero(tableroCords);
+        setMapPiezas(piezas);
+    }, [tableroCords, piezas]);
     function mostrarPath(cord) {
         const pieza = mapPosPiezas[cord];
+        const cordenadas = cord.split("");
+        const letra = cordenadas[0];
+        const numero = Number(cordenadas[1]);
         if (pieza === "p") {
-            console.log("es un peon");
+            //Peon blanco
+            setPosibles([letra.concat(numero + 1), letra.concat(numero + 2)]);
+        } else if (pieza === "P") {
+            //Peon Negro
+            setPosibles([letra.concat(numero - 1), letra.concat(numero - 2)]);
+        } else if (pieza === "c") {
+            //Caballo blanco
+            //Aca tengo que "sumar" letras que creo que lo tengo que hacer con ascii
+        } else if (pieza === "C") {
+            //Caballo negro
         }
     }
-
     return (
         <article className="tablero">
             {tablero &&
@@ -80,6 +46,13 @@ export function Tablero() {
                                     alt={columna}
                                 />
                             )}
+                            {posibles &&
+                                posibles.map(
+                                    (pos) =>
+                                        pos === columna && (
+                                            <span className="posible"></span>
+                                        ),
+                                )}
                         </div>
                     )),
                 )}
---
+++ src/Hooks/useStartTablero.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+
+export function useStartTablero() {
+    const [tableroCords, setTablero] = useState();
+    const [piezas, setPiezas] = useState();
+    useEffect(() => {
+        let tableroAux = Array(8)
+            .fill(null)
+            .map(() => Array(8).fill(""));
+        const arregloUtil = [
+            [8, 7, 6, 5, 4, 3, 2, 1],
+            ["a", "b", "c", "d", "e", "f", "g", "h"],
+        ];
+        arregloUtil[0].forEach((columna, indexcol) => {
+            arregloUtil[1].forEach((fila, indexfila) => {
+                tableroAux[indexcol][indexfila] = `${fila}${columna}`;
+            });
+        });
+        //Seteo el tablero con sus cords
+        setTablero(tableroAux);
+        setPiezas({
+            a1: "r",
+            b1: "c",
+            c1: "b",
+            d1: "q",
+            e1: "k",
+            f1: "b",
+            g1: "c",
+            h1: "r",
+            a2: "p",
+            b2: "p",
+            c2: "p",
+            d2: "p",
+            e2: "p",
+            f2: "p",
+            g2: "p",
+            h2: "p",
+            a7: "P",
+            b7: "P",
+            c7: "P",
+            d7: "P",
+            e7: "P",
+            f7: "P",
+            g7: "P",
+            h7: "P",
+            a8: "R",
+            b8: "C",
+            c8: "B",
+            d8: "Q",
+            e8: "K",
+            f8: "B",
+            g8: "C",
+            h8: "R",
+        });
+    }, []);
+    return { tableroCords, piezas };
+}
