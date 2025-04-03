9ede5cff4d166f64846a1cd07d0fcc7ee416412a
ManuGolem <manuelcarreras2013@gmail.com>
2025-04-02 22:59:32 -0300

    saco la logia de iniciar el tablero al json

--- src/Hooks/useStartTablero.js
+++ src/Hooks/useStartTablero.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import data from "../dictionary.json";
 export function useStartTablero(start) {
     const [tableroCords, setTablero] = useState();
     const [piezas, setPiezas] = useState();
@@ -18,40 +18,7 @@ export function useStartTablero(start) {
         });
         //Seteo el tablero con sus cords
         setTablero(tableroAux);
-        setPiezas({
-            a1: "r",
-            // b1: "c",
-            // c1: "b",
-            // d1: "q",
-            e1: "k",
-            // f1: "b",
-            // g1: "c",
-            h1: "r",
-            // a2: "p",
-            // b2: "p",
-            // c2: "p",
-            // d2: "p",
-            // e2: "p",
-            // f2: "p",
-            // g2: "p",
-            // h2: "p",
-            // a7: "P",
-            // b7: "P",
-            // c7: "P",
-            // d7: "P",
-            // e7: "P",
-            // f7: "P",
-            // g7: "P",
-            // h7: "P",
-            a8: "R",
-            // b8: "C",
-            // c8: "B",
-            // d8: "Q",
-            e8: "K",
-            // f8: "B",
-            // g8: "C",
-            h8: "R",
-        });
+        setPiezas({ ...data.posicionInicial });
     }, [start]);
     return { tableroCords, piezas };
 }
